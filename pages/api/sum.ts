fa4a2865008479f8d49dae49f857ebfd6415c85f
WebriQ <info@webriq.com>
2023-01-30 15:17:58 +0800

    Update sum.ts
    As per Rabby's suggestion, I agree this makes more sense.

--- pages/api/sum.ts
+++ pages/api/sum.ts
@@ -5,5 +5,5 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const { num1, num2 } = req.query;
   const result = sum(num1, num2);
 
-  res.status(200).json({ sum: result });
+  res.status(200).json({ total: result });
 }
